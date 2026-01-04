c937c490af23aabe4928135a11a91bf7bfef2a2f
Rizki Cahya <rizki.cahya03@gmail.com>
2026-01-04 15:37:47 +0700

    feat: card grid column style

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <Image className="dark:invert rounded-full" src="/Villhaze.jpg" alt="logo" width={100} height={20} priority />
+                <Image className="rounded-full" src="/Villhaze.jpg" alt="logo" width={100} height={20} priority />
                 <div className="flex flex-col items-center gap-6 py-5 text-center sm:items-start sm:text-left">
                     <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">Rizki Cahya Ramdani</h1>
                     <span className="">Software Engineering | Web Developer</span>
@@ -25,27 +25,86 @@ export default function Home() {
 
                 <div className="flex flex-col flex-grow w-full gap-6 mt-10">
                     <h1 className="font-bold text-2xl">My Project</h1>
-                    <div className="border-gray-300 border-1 shadow-sm rounded-2xl p-6 sm:w-full">
-                        <h1 className="font-bold text-2xl mb-3">MiraiNime</h1>
-                        <p className="text-justify mb-6">MiraiNime is a simple website that utilizes the Jikan API to display anime information from MyAnimeList.</p>
-                        <a href="https://github.com/rizkicahyaa/mirai-nime.git" target="_blank" className="bg-foreground text-sm text-white px-5 py-2.5 rounded-3xl hover:bg-[#000000b3] md:w-[158px]">
-                            View Project
-                        </a>
+                    <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                        <div className="border-gray-300 border-1 shadow-sm rounded-2xl p-6 sm:w-full">
+                            <h1 className="font-bold text-2xl mb-3">MiraiNime</h1>
+                            <p className="text-justify mb-6 leading-8">MiraiNime is a simple website that utilizes the Jikan API to display anime information from MyAnimeList.</p>
+
+                            <div className="flex flex-col w-full gap-3 text-base text-center font-medium sm:flex-row">
+                                <a href="https://github.com/rizkicahyaa/mirai-nime.git" target="_blank" className="bg-foreground text-sm text-white px-5 py-2.5 rounded-3xl transition hover:bg-[#000000b3] ">
+                                    View Project
+                                </a>
+                                <a href="https://mirainime.vercel.app" target="_blank" className="bg-white text-sm text-black border-1 border-gray-300 px-5 py-2.5 rounded-3xl transition hover:bg-[#00000021]">
+                                    Demo
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="border-gray-300 border-1 shadow-sm rounded-2xl p-6 sm:w-full">
+                            <h1 className="font-bold text-2xl mb-3">Focsy</h1>
+                            <p className="text-justify mb-6 leading-8">A productivity timer web application that helps you stay focused, manage your time, and get more done (pomodoro).</p>
+                            <div className="flex flex-col w-full gap-3 text-base text-center font-medium sm:flex-row">
+                                <a href="https://github.com/rizkicahyaa/mirai-nime.git" target="_blank" className="bg-foreground text-sm text-white px-5 py-2.5 rounded-3xl transition hover:bg-[#000000b3] ">
+                                    View Project
+                                </a>
+                                <a href="https://mirainime.vercel.app" target="_blank" className="bg-white text-sm text-black border-1 border-gray-300 px-5 py-2.5 rounded-3xl transition hover:bg-[#00000021]">
+                                    Demo
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="border-gray-300 border-1 shadow-sm rounded-2xl p-6 sm:w-full">
+                            <h1 className="font-bold text-2xl mb-3">Jogja Culinary</h1>
+                            <p className="text-justify mb-6 leading-8">A culinary website in Yogyakarta with a responsive layout using React, Tailwind CSS, and MySQL</p>
+                            <div className="flex flex-col w-full gap-3 text-base text-center font-medium sm:flex-row">
+                                <a href="https://github.com/rizkicahyaa/mirai-nime.git" target="_blank" className="bg-foreground text-sm text-white px-5 py-2.5 rounded-3xl transition hover:bg-[#000000b3] ">
+                                    View Project
+                                </a>
+                                <a href="https://mirainime.vercel.app" target="_blank" className="bg-white text-sm text-black border-1 border-gray-300 px-5 py-2.5 rounded-3xl transition hover:bg-[#00000021]">
+                                    Demo
+                                </a>
+                            </div>
+                        </div>
                     </div>
+                    {/* <div className="border-gray-300 border-1 shadow-sm rounded-2xl p-6 sm:w-full">
+                        <h1 className="font-bold text-2xl mb-3">MiraiNime</h1>
+                        <p className="text-justify mb-6 leading-8">MiraiNime is a simple website that utilizes the Jikan API to display anime information from MyAnimeList.</p>
+
+                        <div className="flex flex-col w-full gap-3 text-base text-center font-medium sm:flex-row">
+                            <a href="https://github.com/rizkicahyaa/mirai-nime.git" target="_blank" className="bg-foreground text-sm text-white px-5 py-2.5 rounded-3xl transition hover:bg-[#000000b3] ">
+                                View Project
+                            </a>
+                            <a href="https://mirainime.vercel.app" target="_blank" className="bg-white text-sm text-black border-1 border-gray-300 px-5 py-2.5 rounded-3xl transition hover:bg-[#00000021]">
+                                Demo
+                            </a>
+                        </div>
+                    </div>
+
                     <div className="border-gray-300 border-1 shadow-sm rounded-2xl p-6 sm:w-full">
                         <h1 className="font-bold text-2xl mb-3">Focsy</h1>
-                        <p className="text-justify mb-6">A productivity timer web application that helps you stay focused, manage your time, and get more done (pomodoro).</p>
-                        <a href="https://github.com/rizkicahyaa/focsy.git" target="_blank" className="bg-foreground text-sm text-white px-5 py-2.5 rounded-3xl hover:bg-[#000000b3] md:w-[158px]">
-                            View Project
-                        </a>
+                        <p className="text-justify mb-6 leading-8">A productivity timer web application that helps you stay focused, manage your time, and get more done (pomodoro).</p>
+                        <div className="flex flex-col w-full gap-3 text-base text-center font-medium sm:flex-row">
+                            <a href="https://github.com/rizkicahyaa/mirai-nime.git" target="_blank" className="bg-foreground text-sm text-white px-5 py-2.5 rounded-3xl transition hover:bg-[#000000b3] ">
+                                View Project
+                            </a>
+                            <a href="https://mirainime.vercel.app" target="_blank" className="bg-white text-sm text-black border-1 border-gray-300 px-5 py-2.5 rounded-3xl transition hover:bg-[#00000021]">
+                                Demo
+                            </a>
+                        </div>
                     </div>
+
                     <div className="border-gray-300 border-1 shadow-sm rounded-2xl p-6 sm:w-full">
                         <h1 className="font-bold text-2xl mb-3">Jogja Culinary</h1>
-                        <p className="text-justify mb-6">A productivity timer web application that helps you stay focused, manage your time, and get more done (pomodoro).</p>
-                        <a href="https://github.com/rizkicahyaa/jogja-culinary.git" target="_blank" className="bg-foreground text-sm text-white px-5 py-2.5 rounded-3xl hover:bg-[#000000b3] md:w-[158px]">
-                            View Project
-                        </a>
-                    </div>
+                        <p className="text-justify mb-6 leading-8">A culinary website in Yogyakarta with a responsive layout using React, Tailwind CSS, and MySQL</p>
+                        <div className="flex flex-col w-full gap-3 text-base text-center font-medium sm:flex-row">
+                            <a href="https://github.com/rizkicahyaa/mirai-nime.git" target="_blank" className="bg-foreground text-sm text-white px-5 py-2.5 rounded-3xl transition hover:bg-[#000000b3] ">
+                                View Project
+                            </a>
+                            <a href="https://mirainime.vercel.app" target="_blank" className="bg-white text-sm text-black border-1 border-gray-300 px-5 py-2.5 rounded-3xl transition hover:bg-[#00000021]">
+                                Demo
+                            </a>
+                        </div>
+                    </div> */}
                 </div>
 
                 <footer className="text-center mx-auto mt-20">
