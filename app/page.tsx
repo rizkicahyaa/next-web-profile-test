4948e9c553b8a8c16a250b654f4bfb34445df688
Rizki Cahya <rizki.cahya03@gmail.com>
2026-01-05 14:26:02 +0700

    feat: finishing form contact

--- app/page.tsx
+++ app/page.tsx
@@ -73,82 +73,40 @@ export default function Home() {
                 </div>
 
                 <div className="flex flex-col flex-grow w-full gap-6 mt-10">
-                    <div className="border-b border-gray-900/10 pb-12">
+                    <div className="pb-4">
                         <h1 className="font-bold text-2xl text-gray-900">Contact Me</h1>
                         {/* <p className="mt-1 text-sm/6 text-gray-600">Use a permanent address where you can receive mail.</p> */}
                         <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                             <div className="sm:col-span-3">
-                                <label htmlFor="first-name" className="block text-sm/6 font-medium text-gray-900">
-                                    First name
+                                <label htmlFor="name" className="block text-sm/6 font-medium text-gray-900">
+                                    Your name
                                 </label>
                                 <div className="mt-2">
-                                    <input id="first-name" type="text" name="first-name" autoComplete="given-name" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                                    <input id="name" type="text" name="name" autoComplete="given-name" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                                 </div>
                             </div>
                             <div className="sm:col-span-3">
-                                <label htmlFor="last-name" className="block text-sm/6 font-medium text-gray-900">
-                                    Last name
-                                </label>
-                                <div className="mt-2">
-                                    <input id="last-name" type="text" name="last-name" autoComplete="family-name" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-                                </div>
-                            </div>
-                            <div className="sm:col-span-4">
                                 <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
-                                    Email address
+                                    Your email
                                 </label>
                                 <div className="mt-2">
-                                    <input id="email" type="email" name="email" autoComplete="email" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                                    <input id="email" type="email" name="email" autoComplete="family-name" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                                 </div>
                             </div>
-                            <div className="sm:col-span-3">
-                                <label htmlFor="country" className="block text-sm/6 font-medium text-gray-900">
-                                    Country
-                                </label>
-                                <div className="mt-2 grid grid-cols-1">
-                                    <select id="country" name="country" autoComplete="country-name" className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                                        <option>United States</option>
-                                        <option>Canada</option>
-                                        <option>Mexico</option>
-                                    </select>
-                                    <svg viewBox="0 0 16 16" fill="currentColor" data-slot="icon" aria-hidden="true" className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4">
-                                        <path d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" fillRule="evenodd" />
-                                    </svg>
-                                </div>
-                            </div>
-                            <div className="col-span-full">
-                                <label htmlFor="street-address" className="block text-sm/6 font-medium text-gray-900">
-                                    Street address
+                            <div className="sm:col-span-full">
+                                <label htmlFor="message" className="block text-sm/6 font-medium text-gray-900">
+                                    Your message
                                 </label>
                                 <div className="mt-2">
-                                    <input id="street-address" type="text" name="street-address" autoComplete="street-address" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-                                </div>
-                            </div>
-                            <div className="sm:col-span-2 sm:col-start-1">
-                                <label htmlFor="city" className="block text-sm/6 font-medium text-gray-900">
-                                    City
-                                </label>
-                                <div className="mt-2">
-                                    <input id="city" type="text" name="city" autoComplete="address-level2" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-                                </div>
-                            </div>
-                            <div className="sm:col-span-2">
-                                <label htmlFor="region" className="block text-sm/6 font-medium text-gray-900">
-                                    State / Province
-                                </label>
-                                <div className="mt-2">
-                                    <input id="region" type="text" name="region" autoComplete="address-level1" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-                                </div>
-                            </div>
-                            <div className="sm:col-span-2">
-                                <label htmlFor="postal-code" className="block text-sm/6 font-medium text-gray-900">
-                                    ZIP / Postal code
-                                </label>
-                                <div className="mt-2">
-                                    <input id="postal-code" type="text" name="postal-code" autoComplete="postal-code" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                                    <textarea id="about" name="about" rows={3} className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" defaultValue={""} />
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className="mt-6 flex items-center justify-center gap-x-6">
+                        <button type="submit" className="bg-foreground text-sm text-white px-5 py-2.5 rounded-3xl transition hover:bg-[#000000b3] dark:hover:bg-[#ccc]">
+                            Send Message
+                        </button>
                     </div>
                 </div>
 
