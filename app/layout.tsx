dfe1f2fe1ffd30accad53a1fd0e8a6d89a6d4d2a
Rizki Cahya <rizki.cahya03@gmail.com>
2026-01-09 17:55:43 +0700

    update: change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "Rizki Cahya - Profile",
+    title: "@rizkichrmdn",
     description: "Rizki Cahya's Profile",
     // icons: {
     //     icon: [
